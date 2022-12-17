8766e6b0b8a8039758380f54410c72e3a05eb7cd
PRIYAKAMARAJ05 <107319067+PRIYAKAMARAJ05@users.noreply.github.com>
2022-12-17 14:22:23 +0530

    Update majorityElements.c

--- DAY - 1/majorityElements.c
+++ DAY - 1/majorityElements.c
@@ -1,1 +1,16 @@
-
+int majorityElement(int* nums, int numsSize){
+    int num=arr[0];
+    int numCount=1;
+    for(int i=0;i<numsSize;i++){
+        if(arr[i]==num){
+            ++numcounnt;
+        }else{
+            --numcount;
+        }
+        if(numCount==0){
+            num=arr[i];
+            numCount=1;
+        }
+    }
+    return num;
+}
