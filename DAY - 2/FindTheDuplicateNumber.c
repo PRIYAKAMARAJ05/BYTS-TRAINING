52309babd70040d893a1203b230d68da114b0e2b
PRIYAKAMARAJ05 <107319067+PRIYAKAMARAJ05@users.noreply.github.com>
2022-12-17 16:04:44 +0530

    Update FindTheDuplicateNumber.c

--- DAY - 2/FindTheDuplicateNumber.c
+++ DAY - 2/FindTheDuplicateNumber.c
@@ -1,1 +1,12 @@
-
+int findDuplicate(int* nums, int numsSize){
+    int arr[numsSize-1];
+    for(int i=0;i<numsSize-1;i++){
+        arr[i]=0;
+    }
+    for(int i=0;i<=numsSize;i++){
+        arr[nums[i]-1]+=1;
+        if(arr[nums[i]-1]>1)
+            return nums[i];
+    }
+    return 0;
+}
